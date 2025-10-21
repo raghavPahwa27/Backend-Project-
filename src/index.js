6987662257c7b41e44f47b13ed445e08fff2b067
Raghav Pahwa <raghavpahwa2005@gmail.com>
2025-10-22 01:30:17 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -11,6 +11,25 @@ dotenv.config({
 
 import connectDB from "./db/index.js"
 connectDB()
+.then(){
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port ${process.env.PORT}`)
+    })
+}
+.catch((err)=>{
+    console.log("Mongo db conection failed !!!!",err)
+})
+
+
+
+
+
+
+
+
+
+
+
 // const app = express();
 
 // (async () => {
